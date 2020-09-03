f1374eafe0058430b3bd0a9c8ef30126d4f038c3
ktalens <ktal66@yahoo.com>
2020-09-03 04:01:19 -0700

    landscaper: user can use scissors to cut grass

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var dollars = 0;
+var dollars = 4;
 var scissors =false;
-var toolbox = [];
+var toolbox = ["teeth"];
 
 let moneyItem = document.getElementById("money");
 let moneyLog = document.createElement("p");
@@ -18,6 +18,28 @@ teeth.onclick = function(){
     status()//
     //storeUpdate();
 };
+const toolboxUpdate = () => 
+    {
+        toolbox.forEach
+        (
+            (toolboxTool,index) =>
+        {
+            console.log(toolbox);
+            if (index>0)
+                {
+                    let toolSheet=document.getElementById("toolbox");
+                    let tool=document.createElement("button");
+                    tool.textContent=("Use "+toolboxTool);
+                    toolSheet.appendChild(tool);
+                    tool.onclick=function()
+                        {
+                            dollars+= store[index-1].earningPower;
+                            moneyUpdate();
+                        }
+                }
+        }
+        )
+    };
 
 
 
@@ -25,12 +47,14 @@ const store = [
     {
         item: "rusty scissors",
         price: 5,
-        quantity: 1
+        quantity: 1,
+        earningPower: 5
     },
     {
         item: "push lawnmower",
         price: 25,
-        quantity: 1
+        quantity: 1,
+        earningPower: 50
     }
 ];
 
@@ -53,7 +77,7 @@ const storeUpdate = ()=>{
         {if(thing.quantity>0)
             {
                 let storeLog = document.createElement("li");
-                console.log(thing.item);
+                //console.log(thing.item);
                 storeLog.textContent=thing.item;
                 storeShelf.appendChild(storeLog);
                 let stockItem = document.createElement("button");
@@ -66,7 +90,11 @@ const storeUpdate = ()=>{
                         toolbox.push(thing.item);
                         dollars -= thing.price;
                         moneyUpdate();
-                    } else {
+                        toolboxUpdate();
+                        let soldOut=document.getElementById(thing.item);
+                        soldOut.style.display = "none";
+                    }
+                    else {
                         console.log("You only have $"+dollars)
                     }
                     };
@@ -74,6 +102,11 @@ const storeUpdate = ()=>{
                 stockItem.setAttribute("class","buyme");
                 storeLog.appendChild(stockItem);
                 storeShelf.appendChild(storeLog);
+            } else if(item.quantity===0)
+            {
+                console.log(item.quantity);
+                //let soldOut=document.getElementById("rusty scissors");
+                //storeShelf.parentNode.removeChild(soldOut);
             } else {
                 return
             } 
